da6044c83bd1a53610e8b615f19d4af190ead42f
adhishVerma <v.adhish16@gmail.com>
2023-02-28 00:29:35 +0530

    add : authContext

---
+++ client/src/context/authContext.js
@@ -0,0 +1,26 @@
+import { createContext, useEffect, useState } from "react";
+
+export const AuthContext = createContext();
+
+export const AuthContextProvider = ({children}) => {
+  const [currentUser, setCurrentUser] = useState(
+    JSON.parse(localStorage.getItem("user") || null)
+  );
+  
+  const login = () => {
+    // TO DO
+  }
+
+  const logout = () => {
+    // TO DO
+  }
+
+  useEffect(() => {
+    localStorage.setItem("user", JSON.stringify(currentUser))
+  },[currentUser])
+
+  return (<AuthContext.Provider value={{login,currentUser,logout}}>
+    {children}
+  </AuthContext.Provider>)
+
+}
